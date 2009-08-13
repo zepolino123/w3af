f20d137d250a85b563915888b10bb4cfa6ab5957
oxdef <oxdef@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2009-08-13 20:31:49 +0000

    HistoryItem => active record pattern History => interact with DB + factory for new items

--- core/data/db/history.py
+++ core/data/db/history.py
@@ -37,19 +37,19 @@ import core.controllers.outputManager as om
 import re
 import os
 from core.controllers.w3afException import w3afException
-from core.data.db.persist import persist
-
-from core.data.db.persist import persist
+from core.data.db.db import DB
 from core.controllers.misc.homeDir import get_home_dir
 import core.data.kb.config as cf
 
 class History:
-    """Provide convinient way to access history."""
+    '''Provides convinient way to access history.'''
 
     def __init__(self):
-        pass
+        '''Construct object.'''
+        self._db = None
 
     def begin(self):
+        '''Create DB and add tables.'''
         sessionName = cf.cf.getData('sessionName')
         dbName = os.path.join(get_home_dir(), 'sessions', 'db_' + sessionName)
         # Just in case the directory doesn't exist...
@@ -61,7 +61,7 @@ class History:
                 msg = 'Unable to write to the user home directory: ' + get_home_dir()
                 raise w3afException( msg )
 
-        self._db = persist()
+        self._db = DB()
         # Check if the database already exists
         if os.path.exists(dbName):
             # Find one that doesn't exist
@@ -70,24 +70,24 @@ class History:
                 if not os.path.exists(newDbName):
                     dbName = newDbName
                     break
-        # Create one!
-        self._db.create(dbName, ['id', 'url', 'code'])
+        # Create DB!
+        self._db.open(dbName)
+        # Create table
+        self._db.createTable('data_table', [('id','integer'), ('url', 'text'),
+            ('code', 'text'), 'raw_pickled_data', 'blob'], ['id',])
 
     def end(self):
-        pass
-    def search(self, data):
-        pass
-    def searchById(self, id):
-        pass
-    def searchAll(self, data):
-        pass
+        '''End history.'''
+        self._db.close()
+
     def getNewItem(self):
+        '''Factory for new items.'''
         newItem = HistoryItem(self._db)
-        print "getNewItem"
         return newItem
 
 class HistoryItem:
     _db = None
+    _dataTable = 'data_table'
     id = None
     request = None
     response = None
@@ -95,15 +95,90 @@ class HistoryItem:
     mark = False
 
     def __init__(self, db=None):
+        '''Construct object.'''
         if db:
             self._db = db
 
-    def read(self):
-        pass
+    def find(self, searchData, resultLimit=-1, orderData=[]):
+        '''Make complex search.
+        search_data = [(name, value, operator), ...]
+        '''
+        if not self._db:
+            raise w3afException('The database is not initialized yet.')
+        sql = 'SELECT * FROM ' + self._dataTable
+        where = ' WHERE 1=1 '
+        result = []
+        # FIXME add prepared statements!
+        for item in searchData:
+            oper = "="
+            value = item[1]
+            if len(item) > 2:
+                oper = item[2]
+            if isinstance(value, str):
+                value = "'" + value + "'"
+            else:
+                value = str(value)
+            where += " AND (" + item[0] + " " + oper + " " + value + ")"
+
+        orderby = ""
+        for item in orderData:
+            orderby += item[0] + " " + item[1] + ","
+        orderby = orderby[:-1]
+
+        if orderby:
+            sql += " ORDER BY " + orderby
+
+        sql += ' LIMIT '  + str(resultLimit)
+
+        try:
+            rawResult = self._db.retrieveAll(sql)
+            for row in rawResult:
+                item = self._db.getNewItem()
+                f = StringIO(str(row[-1]))
+                req, res = Unpickler(f).load()
+                item.id = res.getId()
+                item.request = req
+                item.response = res
+                result.append(item)
+        except w3afException:
+            raise w3afException('You performed an invalid search. Please verify your syntax.')
+        return result
+
+    def load(self, id=None):
+        '''Load data from DB by ID.'''
+        if not self._db:
+            raise w3afException('The database is not initialized yet.')
+
+        if not id:
+            id = self.id
+
+        sql = 'SELECT * FROM ' + self._dataTable + ' WHERE id = ? '
+        try:
+            row = self._db.retrieve(sql, (id,))
+            f = StringIO(str(row[-1]))
+            req, res = Unpickler(f).load()
+            self.request = req
+            self.response = res
+        except w3afException:
+            raise w3afException('You performed an invalid search. Please verify your syntax.')
+        return True
 
     def save(self):
+        # Insert 
         if not self.id:
-            self._db.persist((self.response.getId(), self.request.getURI(),
-                self.response.getCode()), (self.request, self.response))
+            values = []
+            values.append(self.response.getId())
+            values.append(self.request.getURI())
+            values.append(self.request.getCode())
+            f = StringIO()
+            p = Pickler(f)
+            p.dump((self.request, self.response))
+            values.append(f.getvalue())
+            sql = 'INSERT INTO ' + self._dataTable + ' (id, url, code, raw_pickled_data)'
+            sql += ' VALUES (?,?,?,?)'
+            self._db.execute(sql, values)
             self.id = self.response.getId()
+        else:
+            # Update
+            pass
         return True
